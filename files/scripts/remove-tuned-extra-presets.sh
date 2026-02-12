8567f27825571448576010d8243d9cc72ed243b3
zodium#dev <dev.zodium.project@gmail.com>
2026-02-12 16:51:48 +0530

    remove bloat

---
+++ files/scripts/remove-tuned-extra-presets.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+cd /usr/lib/tuned/profiles/
+ls
+rm -rf /usr/lib/tuned/profiles/accelerator-performance/
+rm -rf /usr/lib/tuned/profiles/hpc-compute/
+rm -rf /usr/lib/tuned/profiles/latency-performance/
+rm -rf /usr/lib/tuned/profiles/powersave/
+rm -rf /usr/lib/tuned/profiles/throughput-performance/
+rm -rf /usr/lib/tuned/profiles/virtual-guest/
+rm -rf /usr/lib/tuned/profiles/virtual-host/
+rm -rf /usr/lib/tuned/profiles/desktop/
+rm -rf /usr/lib/tuned/profiles/optimize-serial-console/
+rm -rf /usr/lib/tuned/profiles/network-latency/
+rm -rf /usr/lib/tuned/profiles/network-throughput/
+rm -rf /usr/lib/tuned/profiles/intel-sst/
+rm -rf /usr/lib/tuned/profiles/aws/
+rm -rf /usr/lib/tuned/profiles/balanced/
+rm -rf /usr/lib/tuned/profiles/balanced-battery/
+ls
