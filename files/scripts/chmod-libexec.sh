d067e18901b2f8abacb1424fe23a1be702087b9d
zodium#dev <dev.zodium.project@gmail.com>
2026-02-12 17:37:28 +0530

    fix

--- files/scripts/chmod-libexec.sh
+++ files/scripts/chmod-libexec.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 set -x
 
-chmod 0755 /usr/libexec/intel-hwp-start.sh
-chmod 0755 /usr/libexec/intel-hwp-stop.sh
+chmod 0755 /usr/libexec/intel-hwp-dynamic-boost-start.sh
+chmod 0755 /usr/libexec/intel-hwp-dynamic-boost-stop.sh
 chmod 0755 /usr/libexec/zodium-set-gamemode.sh
 chmod 0755 /usr/bin/zupdate
