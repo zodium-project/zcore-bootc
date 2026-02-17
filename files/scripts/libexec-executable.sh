f2f83de35d8b00ae76547815c7f7207c2b832779
zodium#dev <dev.zodium.project@gmail.com>
2026-02-18 00:09:23 +0530

    make them executable

--- files/scripts/libexec-executable.sh
+++ files/scripts/libexec-executable.sh
@@ -6,3 +6,5 @@ chmod 0755 /usr/libexec/zodium-intel-hwp-dynamic-boost-start.sh
 chmod 0755 /usr/libexec/zodium-intel-hwp-dynamic-boost-stop.sh
 chmod 0755 /usr/libexec/zodium-set-gamemode.sh
 chmod 0755 /usr/bin/zupdate
+chmod 0755 /usr/bin/prime-run
+chmod 0755 /usr/bin/dgpu-run-experimental
