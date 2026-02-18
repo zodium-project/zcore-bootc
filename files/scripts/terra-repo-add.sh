90aa1f3ac90ad3e292980b9b913a12c8def71219
zodium#dev <dev.zodium.project@gmail.com>
2026-02-18 16:33:37 +0530

    better ( simplify )

--- files/scripts/terra-repo-add.sh
+++ files/scripts/terra-repo-add.sh
@@ -4,5 +4,7 @@ set -oue pipefail
 
 echo 'this is terra repo setup script'
 dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
-dnf upgrade -y
+dnf upgrade --refresh -y
+dnf reinstall --refresh -y terra-release
+dnf upgrade --refresh -y
 echo 'terra repo setup script completed'
---
+++ files/scripts/vscode-repo-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
