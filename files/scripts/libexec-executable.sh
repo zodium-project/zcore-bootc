7112a5a187be5ca3aa6cb7d5566485830c2f964d
zodium@dev <dev.zodium.project@gmail.com>
2026-02-19 19:01:40 +0530

    fix

--- files/scripts/libexec-executable.sh
+++ files/scripts/libexec-executable.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 set -x
 
-chmod 0755 /usr/libexec/zodium-set-gamemode.sh
+chmod 0755 /usr/libexec/zodium-useradd-gamemode.sh
 chmod 0755 /usr/bin/zupdate
 chmod 0755 /usr/bin/prime-run-experimental
 chmod 0755 /usr/bin/dgpu-run-experimental
