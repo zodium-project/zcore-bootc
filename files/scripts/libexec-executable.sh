32d8f044b30d229b39c57a55a6e7250d87e6140a
zodium@dev <dev.zodium.project@gmail.com>
2026-02-19 18:19:37 +0530

    fix

--- files/scripts/libexec-executable.sh
+++ files/scripts/libexec-executable.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 set -x
 
-chmod 0755 /usr/libexec/zodium-intel-hwp-dynamic-boost-start.sh
-chmod 0755 /usr/libexec/zodium-intel-hwp-dynamic-boost-stop.sh
 chmod 0755 /usr/libexec/zodium-set-gamemode.sh
 chmod 0755 /usr/bin/zupdate
 chmod 0755 /usr/bin/prime-run-experimental
