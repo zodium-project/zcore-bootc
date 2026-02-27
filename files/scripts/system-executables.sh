bfe0b8e884e5eeace7b1a693ffc996b1dc411a98
dev@zodium <dev.zodium.project@gmail.com>
2026-02-27 16:49:34 +0530

    remove prime-run

--- files/scripts/system-executables.sh
+++ files/scripts/system-executables.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 
 chmod 0755 /usr/libexec/zodium-useradd-gamemode.sh
-chmod 0755 /usr/bin/prime-run-experimental
 chmod 0755 /usr/bin/dgpu-run-experimental
 chmod 0755 /usr/libexec/zodium-tuned-sync.sh
