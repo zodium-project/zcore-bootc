54b6c67da308d41738a75ef5887acdafb97ed056
dev@zodium <dev.zodium.project@gmail.com>
2026-03-01 18:10:16 +0530

    fix builds

--- files/scripts/system-executables.sh
+++ files/scripts/system-executables.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-chmod 0755 /usr/libexec/zodium-tuned-sync.sh
-chmod 0755 /usr/libexec/zodium-useradd-gamemode.sh
-chmod 0755 /usr/bin/dgpu-run
+# Make Tools Executable
+
+chmod 0755 /usr/bin/gpu-run
 chmod 0755 /usr/bin/zust
 chmod 0755 /usr/bin/zync
+
+# Make Scripts Executable
+
+chmod 0755 /usr/libexec/zodium-tuned-sync.sh
+chmod 0755 /usr/libexec/zodium-useradd-gamemode.sh
+chmod 0755 /usr/lib/zust-scripts/*
