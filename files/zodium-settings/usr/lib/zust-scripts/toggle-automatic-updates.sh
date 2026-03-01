8cd45ff0d6313c1bdcd2448a452396c74acf2da2
dev@zodium <dev.zodium.project@gmail.com>
2026-03-02 02:14:34 +0530

    done

---
+++ files/zodium-settings/usr/lib/zust-scripts/toggle-automatic-updates.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# ================================================================
+#  zync-auto-updates — Manage automatic updates for zync
+# ================================================================
+set -euo pipefail
+
+/usr/bin/zync --auto-updates
