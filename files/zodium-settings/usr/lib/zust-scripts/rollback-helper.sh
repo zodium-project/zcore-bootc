571844ae58e8837c7c5fa25965380e2f5dc2b5b0
dev@zodium <dev.zodium.project@gmail.com>
2026-03-02 02:28:35 +0530

    added zust script

--- files/zodium-settings/usr/lib/zust-scripts/rollback-helper.sh
+++ files/zodium-settings/usr/lib/zust-scripts/rollback-helper.sh
@@ -1,2 +1,7 @@
-#wip
-# not usable atm
+#!/usr/bin/env bash
+# ================================================================
+#  zync-auto-updates — Manage automatic updates for zync
+# ================================================================
+set -euo pipefail
+
+/usr/bin/zync --rollback
